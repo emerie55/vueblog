3ffc5f05a41281be80174a9c3da31d409372b44f
ebuberoderick <ebuberoderick2@gmail.com>
2021-09-14 18:52:40 +0100

    work on the blog post

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,11 +7,11 @@ import { InertiaProgress } from '@inertiajs/progress';
 // Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faHome, faInfoCircle, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
+import { faBasketballBall, faEllipsisH, faHome, faInfoCircle, faLayerGroup, faShareAlt, faStarOfLife, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faComments, faEdit } from '@fortawesome/free-regular-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faHome, faInfoCircle, faComments, faFacebook, faEdit, faLayerGroup)
+library.add(faHome, faInfoCircle, faTimes, faComments, faFacebook, faEdit, faLayerGroup, faBasketballBall, faEllipsisH, faStarOfLife, faShareAlt, faComments)
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
