b27ce68f36cf4dafc12474856f14e82f5278c6f6
peterchigozirim <peterchigozirim@gmail.com>
2021-09-14 19:52:34 +0100

    makepost update

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,10 +8,10 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faHome, faInfoCircle, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
-import { faComments, faEdit } from '@fortawesome/free-regular-svg-icons';
+import { faComments, faEdit, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faHome, faInfoCircle, faComments, faFacebook, faEdit, faLayerGroup)
+library.add(faHome, faInfoCircle, faComments, faFacebook, faEdit, faLayerGroup,faTrashAlt)
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
